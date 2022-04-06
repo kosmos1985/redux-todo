e324d910c0b340f4d9e2e9c2fa88593fcb94b8f9
maciej.kosmela <maciej.kosmela@enigma.com.pl>
2022-04-06 08:42:19 +0200

    I added action.ts and completed the reducer function with the appropriate cases

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,3 +1,4 @@
+import { ADD_TODO, REMOVE_ALL_TODOS, REMOVE_TODO, TOGGLE_TODO } from './action';
 import { ITodo } from './interfaces/todo';
 
 export interface IAppState {
@@ -11,5 +12,36 @@ export const INITIAL_STATE: IAppState = {
 };
 
 export function rootReducer(state: any, action: any) {
+  switch (action.type) {
+    case ADD_TODO:
+      action.todo.id = state.todos.length + 1;
+      return Object.assign({}, state, {
+        todos: state.todos.concat(Object.assign({}, action.todo)),
+        lastUpdate: new Date(),
+      });
+    case TOGGLE_TODO:
+      let todo = state.todos.find((todo: { id: any }) => todo.id === action.id);
+      let index = state.todos.indexOf(todo);
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos.slice(0, index),
+          Object.assign({}, state, { isComplited: !todo.isComplited }),
+          ...state.todos.slice(index + 1),
+        ],
+        lastUpdate: new Date(),
+      });
+
+    case REMOVE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.filter((todo: { id: any }) => todo.id !== action.id),
+        lastUpdate: new Date(),
+      });
+
+    case REMOVE_ALL_TODOS:
+      return Object.assign({}, state, {
+        todos: [],
+        lastUpdate: new Date(),
+      });
+  }
   return state;
 }
